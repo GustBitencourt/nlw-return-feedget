11a84bdfdeaa30fc83c8da0a96c7b89f9e829892
GustBitencourt <gubiten@gmail.com>
2022-05-04 21:47:45 -0300

    funcionalidade de screenshot e possibilidade de exclui-la alem de componente de loading

--- nlw-front/src/components/WidgetForm/components/FeedbackContent/index.tsx
+++ nlw-front/src/components/WidgetForm/components/FeedbackContent/index.tsx
@@ -1,13 +1,22 @@
-import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
+import { ArrowLeft } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "../..";
 import { CloseButton } from "../../../CloseButtom"
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentProps {
     feedbackType: FeedbackType;
     onFeedbackRestartRequested: () => void;
 }
 
-export const FeedbackContent = ({ feedbackType, onFeedbackRestartRequested }: FeedbackContentProps) => {
+export const FeedbackContent = ({ 
+    feedbackType, 
+    onFeedbackRestartRequested 
+    }: FeedbackContentProps) => {
+
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
 
@@ -41,12 +50,10 @@ export const FeedbackContent = ({ feedbackType, onFeedbackRestartRequested }: Fe
                 />
 
                 <footer className="flex gap-2 mt-2">
-                    <button
-                        type="button"
-                        className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-                    >
-                        <Camera className="w-6 h-6"/>
-                    </button>
+                    <ScreenshotButton
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
 
                     <button
                         type="submit"
