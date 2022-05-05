d2f6d73ada1d636701966a7fc10a826eec2cdaf8
GustBitencourt <gubiten@gmail.com>
2022-05-05 14:38:57 -0300

    config prisma

--- nlw-server/src/server.ts
+++ nlw-server/src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/bbmp', (req, res) => {
+    return res.send('Hello World!');
+})
+
 app.listen(3333, () => {
     console.log('HTTP Server started on port 3333');
 })
