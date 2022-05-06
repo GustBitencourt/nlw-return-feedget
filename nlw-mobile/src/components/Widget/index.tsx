c11ed687067552f1f4b2f75f0cb3c4e7f02e9a66
GustBitencourt <gubiten@gmail.com>
2022-05-06 15:19:56 -0300

    Aula 04  estrtura inicial do projeto de MOBILE

---
+++ nlw-mobile/src/components/Widget/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export const Widget = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Vai Corinthians!</Text>
+    </View>
+  )
+}
