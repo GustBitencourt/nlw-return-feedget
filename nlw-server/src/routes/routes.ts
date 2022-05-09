f74d3d052733780619c232e92ab640557a8f65e5
GustBitencourt <gubiten@gmail.com>
2022-05-09 20:59:50 -0300

    try catch para ver erro de deploy

--- nlw-server/src/routes/routes.ts
+++ nlw-server/src/routes/routes.ts
@@ -1,31 +1,34 @@
-import { PrismaFeedbacksRepository } from './../repositories/prisma/prisma-feedback-repository';
-import { SubmitFeedbackUseCase } from './../use-cases/submit-feedback-use-case';
-import express from 'express';
-import { NodeMailerMailAdapter } from '../adapters/nodemaler/nodemailer-mail-adapter';
+import { PrismaFeedbacksRepository } from "./../repositories/prisma/prisma-feedback-repository";
+import { SubmitFeedbackUseCase } from "./../use-cases/submit-feedback-use-case";
+import express from "express";
+import { NodeMailerMailAdapter } from "../adapters/nodemaler/nodemailer-mail-adapter";
 
 export const routes = express.Router();
 
 //req é onde mandaremos o json do nosso feedback
-routes.post('/feedbacks', async (req, res) => {
+routes.post("/feedbacks", async (req, res) => {
+  //para utilizarmos short sintax abaixo
+  const { type, comment, screenshot } = req.body;
 
-    //para utilizarmos short sintax abaixo
-    const { type, comment, screenshot } = req.body;
-
+  try {
     //parametros com responsabilidades divididas
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-    const nodeMailerMailAdapter = new NodeMailerMailAdapter()
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    const nodeMailerMailAdapter = new NodeMailerMailAdapter();
 
     //função de enviar feedback
     const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodeMailerMailAdapter
-    )
+      prismaFeedbacksRepository,
+      nodeMailerMailAdapter
+    );
 
     await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot,
-    })    
+      type,
+      comment,
+      screenshot,
+    });
 
     return res.status(201).send();
-})
+  } catch (err) {
+    console.log(err);
+  }
+});
